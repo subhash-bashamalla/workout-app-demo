ecef1f1e8b75e6492474541805d984965d08cea1
raging-pheonix <raging@pheonix.com>
2025-10-04 14:36:02 +0530

    Removed tf files

--- terraform/docker-images.tf
+++
@@ -1,7 +0,0 @@
-resource "null_resource" "init_docker_images" {
-  provisioner "local-exec" {
-    command = "make docker_images"
-  }
-
-  depends_on = [google_project_service.container_registry]
-}
--- terraform/service/outputs.tf
+++
@@ -1,7 +0,0 @@
-output url {
-  value = google_cloud_run_service.service.status[0].url
-}
-
-output endpoint {
-  value = "${trimprefix(google_cloud_run_service.service.status[0].url, "https://")}:443"
-}
